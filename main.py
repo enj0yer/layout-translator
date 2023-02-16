51e71f61cc0f0eafa6f81d2bff87520c170fd110
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-16 15:45:29 +0400

    - Change imports and call arguments

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget
 
-from logic import convert, Layouts
+from logic import convert, Languages
 
 app = QApplication(sys.argv)
 window = QWidget()
 window.show()
 app.exec()
 
-print(convert("Руддщб цщкдв!!!", Layouts.EN))
+print(convert("Руддщб цщкдв!!!", Languages.RU))
 
-print(convert("Ghbdtn? vbh!!!", Layouts.RU))
+print(convert("Ghbdtn? vbh!!!", Languages.EN))
